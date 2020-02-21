3da4244a03f5c3a5e98e33f759b926cf637b0599
Luis Roman <louisroman133@gmail.com>
2020-02-21 01:29:33 -0500

    lots of react fundamentals learned here

---
+++ my-app/src/AddNinja.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+
+class AddNinja extends Component {
+    state = {
+        name: null, 
+        age: null, 
+        belt: null
+    }
+
+    handleChange = (e) =>{
+        this.setState({
+           [e.target.id]: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addNinjas(this.state);
+    }
+    render(){
+        return(
+            <div className="formName">
+                <form onSubmit={this.onSubmit}>
+                    <label htmlFor="name">Name : </label>
+                    <input type="text" id="name" onChange={this.handleChange}/>
+                    <label htmlFor="age">Age : </label>
+                    <input type="text" id="age" onChange={this.handleChange}/>
+                    <label htmlFor="belt">Belt : </label>
+                    <input type="text" id="belt" onChange={this.handleChange}/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddNinja;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: "Ryu", age: 30, belt: "black", id: 1 },
+      { name: "Yoshi", age: 20, belt: "green", id: 2 },
+      { name: "Benedict Cumberbatch", age: 43, belt: "Old black", id: 3 }
+    ]
+  }
+
+  addNinjas = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas,
+    })
+  } 
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return(ninja.id !== id)
+    })
+
+    this.setState({
+      ninjas: ninjas,
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to React app</h1>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/>
+        <AddNinja addNinjas = {this.addNinjas}/>
+      </div>
+    );
+  }
 }
 
 export default App;
