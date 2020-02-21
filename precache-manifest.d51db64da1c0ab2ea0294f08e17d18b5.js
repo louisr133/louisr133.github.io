b17679a4a2acd8d389a965179af38eb912ca11c2
Luis Roman <louisroman133@gmail.com>
2020-02-21 09:32:19 -0500

    Updates

---
+++ precache-manifest.d51db64da1c0ab2ea0294f08e17d18b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d386f31e062ad8222421905a3d8c5de",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "330d10979ab61df55ad1",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "32b87b2b6af34cf2fe68",
+    "url": "/my-app/static/js/2.5dd8428c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.5dd8428c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330d10979ab61df55ad1",
+    "url": "/my-app/static/js/main.59ab7215.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
